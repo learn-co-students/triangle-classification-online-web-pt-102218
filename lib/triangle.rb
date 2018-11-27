5145d1ea88fed5c6b319840ae4d938808577b88f
Matthew McCormack <mmccormack1182@gmail.com>
2018-11-27 09:31:16 -0500

    Tests Passed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize( side_1, side_2, side_3)
+    self.side_1 = side_1
+    self.side_2 = side_2
+    self.side_3 = side_3
+  end
+
+  def kind
+    if @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1 || @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+      begin
+        raise TriangleError
+      end
+    elsif @side_1 == @side_2 && @side_1 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      return :isosceles
+    elsif @side_1 != @side_2 && @side_1 != @side_3
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
