2ac543f5a6fadad7c9f9566515d093c251810652
grant smith <grantsmith1256@gmail.com>
2018-12-01 18:42:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,14 @@ class Triangle
   end
   
   def kind
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+  if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
     raise TriangleError
+    elsif @side1 + @side2 > @side3 || @side2 + @side3 > @side1 || @side1 + @side3 > @side2
+    begin
+    raise TriangleError
+    rescue TriangleError => error
+    puts error.message
+  end
   end
     if @side1 != @side2 && @side2 != @side3 && @side3 != @side1
       :scalene
@@ -21,10 +27,9 @@ class Triangle
       :equilateral
     else 
       :isosceles
-    end
   end
-  #nil or 0, side cant be <= 0
-  #check if its legal first before conditionals
+  end
+  
   
   
   class TriangleError < StandardError
