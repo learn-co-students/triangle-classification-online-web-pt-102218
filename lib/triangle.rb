23f1be82107d25edec06e33271d6f8db92d65709
Linda Chang <lindachang186@gmail.com>
2018-12-01 19:09:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_accessor :s1, :s2, :s3
+
+  @@triangle = []
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+    @@triangle << self
+  end
+
+  def self.all
+    @@triangle
+  end
+
+  def kind
+    if @s1 == @s2 && @s1 == @s3
+      :equilateral
+    elsif
+      @s1 == @s2 && @s1 != @s3 && @s2 != @s3 || @s2 == @s3 && @s1 != @s2 && @s3 != @s1 || @s3 == @s1 && @s1 != @s2 && @s3 != @s2
+        :isosceles
+      elsif
+        @s1 != @s2 && @s2 != @s3 && @s1 != @s3
+        :scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+    "This is not a valid triangle."
+  end
+
 end
