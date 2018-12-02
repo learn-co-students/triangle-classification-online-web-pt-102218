59cf94c4a4f25f9a93b935a21727ae00d08d06fb
alexandercleasby <cleasby.alex@gmail.com>
2018-12-01 21:35:08 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
   # write code here
+
+  attr_accessor :kind
+
+  class TriangleError < StandardError
+    
+  end
+  def initialize(a,b,c)
+    arr = [a,b,c].sort
+    if arr[0]<=0 || arr.sort[2]>=arr[0]+arr[1]
+      raise TriangleError
+    end
+    case arr.uniq.length
+    when 1
+      @kind = :equilateral
+    when 2
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
 end
