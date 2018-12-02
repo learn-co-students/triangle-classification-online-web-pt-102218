534e9ca2f9e5023d7568803f202f535f93e0dfe1
Linda Chang <lindachang186@gmail.com>
2018-12-02 15:33:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,6 +7,21 @@ class Triangle
   @@triangle = []
 
   def initialize(s1, s2, s3)
+
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+     raise TriangleError
+   end
+
+   if s1 <= 0 || s2 <= 0 ||s3 <= 0
+     raise TriangleError
+   end
+
+   @@triangle.each do |side|
+     if side == nil
+     raise TriangleError
+    end
+   end
+
     @s1 = s1
     @s2 = s2
     @s3 = s3
@@ -26,8 +41,10 @@ class Triangle
       elsif
         @s1 != @s2 && @s2 != @s3 && @s1 != @s3
         :scalene
-    end
+      end
   end
+
+
 
 
   class TriangleError < StandardError
