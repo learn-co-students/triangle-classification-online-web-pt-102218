62e4fff8cfe501056a660f8f4161ef1cfef29ac8
Jane Venter <janeanneventer@gmail.com>
2018-12-06 22:14:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+  
+  def not_a_triangle 
+    if @a <= 0 || @b <= 0 || @c <= 0 
+      true
+    elsif @a + @b <= @c || @b + @c <= @a || @c + @a <= @b
+      true 
+    else 
+      false 
+    end
+  end
+  
+  def kind 
+    if not_a_triangle == true
+      raise TriangleError 
+    elsif @a == @b && @b == @c 
+      :equilateral 
+    elsif @a == @b || @b == @c || @c == @a 
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError 
+    def message 
+      "This is not a triangle"
+    end
+  end
+  
 end
