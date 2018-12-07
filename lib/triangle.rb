562459cb207e1f17ac503b439a2a2d97250494d4
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-12-07 02:17:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor :num, :num1, :num2
+  
+  
+  def initialize(num, num1, num2)
+    @num = num 
+    @num1 = num1 
+    @num2 = num2 
+  end 
+
+  def kind 
+   
+    raise TriangleError unless is_valid? 
+      
+    
+    if num == num1 && num == num2 
+      :equilateral
+    elsif num == num1 || num == num2 || num1 == num2 
+      :isosceles 
+    else :scalene
+      
+    end 
+    
+  end
+  
+  def is_valid?
+    num  > 0 && num1 > 0 && num2 > 0 &&
+    num + num1 > num2 && num + num2 > num1 && num1 + num2 > num 
+    
+  end 
+  
+  class TriangleError < StandardError
+    
+  end 
+end 
+
+
+
